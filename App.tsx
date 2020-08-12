4ac957900f93558a0144d67ce9e292fca1836326
John Judeh <johnjudeh@hotmail.com>
2020-08-12 21:48:34 +0100

    feat: Add a global dark-content StatusBar in the App component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { View, StyleSheet, StatusBar } from 'react-native';
 import MapperView from './components/MapperView';
 
 function App() {
     return (
-        <MapperView />
+        <View style={styles.container}>
+            <StatusBar barStyle='dark-content' />
+            <MapperView />
+        </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    }
+});
 
 export default App;
