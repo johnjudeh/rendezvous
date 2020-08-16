dff67cfa72d4cc7af83e4d3bd36da871220030e7
John Judeh <johnjudeh@hotmail.com>
2020-08-16 01:36:36 +0100

    feat: Add more theme colours to the Color enum

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,6 +1,14 @@
 enum Color {
+    BLACK = '#070707',
     DARK_GREY = '#353745',
+    MID_GREY = '#6A6E8A',
     LIGHT_GREY = '#D1CFC7',
+    ORANGE = '#E07A5F',
+    DARK_GREEN = '#5A7D7C',
+    LIGHT_GREEN = '#C3D898',
+    LIGHT_BLUE = '#BFD9D7',
+    DARK_RED = '#6D3B47',
+    PLATINUM = '#EDEBE9',
     WHITE = '#FFF',
 }
 
