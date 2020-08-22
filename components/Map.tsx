e440813dda8f9dec27cc491b9a71dc6fc1a2065a
John Judeh <johnjudeh@hotmail.com>
2020-08-22 19:51:56 +0100

    feat: Update MapView to a stateful class component. It asks for permission to see the user location and centers the map on it. Still needs tidying up

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,19 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, EventUserLocation } from 'react-native-maps';
+import * as Location from 'expo-location';
+import Coordinates from '../constants/cities';
 
-function Map() {
-    return (
-        <MapView style={styles.mapStyle}
-            initialRegion={{
-                latitude: 51.5027,
-                longitude: -0.1173,
-                latitudeDelta: 0.1000,
-                longitudeDelta: 0.1000,
-            }}
-            provider={PROVIDER_GOOGLE}
-        />
-    );
+class Map extends Component {
+    state = {
+        userLocation: {
+            ...Coordinates.London,
+        }
+    }
+
+    componentDidMount() {
+        this.requestLocationPermission();
+    }
+
+    requestLocationPermission() {
+        Location.requestPermissionsAsync();
+    }
+
+    onUserLocationChange = (e: EventUserLocation) => {
+        const { coordinate } = e.nativeEvent;
+
+        this.setState({
+            userLocation: {
+                latitude: coordinate.latitude,
+                longitude: coordinate.longitude,
+            }
+        })
+    }
+
+    render() {
+        const { userLocation } = this.state;
+
+        return (
+            <MapView style={styles.mapStyle}
+                initialRegion={{
+                    ...Coordinates.London,
+                    latitudeDelta: 0.1000,
+                    longitudeDelta: 0.1000,
+                }}
+                region={{
+                    ...userLocation,
+                    latitudeDelta: 0.1000,
+                    longitudeDelta: 0.1000,
+                }}
+                provider={PROVIDER_GOOGLE}
+                // TODO: Need to add config before deploying this see intructions in the repo:
+                // https://github.com/react-native-community/react-native-maps/blob/master/docs/mapview.md
+                showsUserLocation={true}
+                showsMyLocationButton={true}
+                onUserLocationChange={this.onUserLocationChange}
+            />
+        );
+    }
 }
 
 const styles = StyleSheet.create({
---
+++ constants/cities.ts
@@ -0,0 +1,10 @@
+import { LatLng } from 'react-native-maps';
+
+const CITY_COORDINATES: { [city: string]: LatLng } = {
+    London: {
+        latitude: 51.5027,
+        longitude: -0.1173,
+    },
+}
+
+export default CITY_COORDINATES;
