03da0e99fa1237bbe8665d7c750c950ce28e6f1c
John Judeh <johnjudeh@hotmail.com>
2020-09-12 18:58:21 +0100

    feat: Add redux to app through redux toolkit and create store with inital reducer

--- App.tsx
+++ App.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
 import MainNavigaton from './components/MainNavigaton';
+
+const store = configureStore({
+    reducer,
+});
 
 function App() {
     return (
-        <View style={styles.container}>
-            <StatusBar style='dark' />
-            <MainNavigaton />
-        </View>
+        <Provider store={store}>
+            <View style={styles.container}>
+                <StatusBar style='dark' />
+                <MainNavigaton />
+            </View>
+        </Provider>
     );
 }
 
