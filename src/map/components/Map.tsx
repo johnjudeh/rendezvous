34aabe83df0001fd6fe85635ffd4c3f45217ab19
John Judeh <johnjudeh@hotmail.com>
2020-09-20 21:09:09 +0100

    feat: Change Map component to be functional component with state hooks. Add jump to current location once after load

--- src/map/components/Map.tsx
+++ src/map/components/Map.tsx
@@ -1,65 +1,53 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useRef, MutableRefObject } from 'react';
 import { StyleSheet } from 'react-native';
-import MapView, { PROVIDER_GOOGLE, EventUserLocation } from 'react-native-maps';
-import * as Location from 'expo-location';
+import MapView, { PROVIDER_GOOGLE, EventUserLocation, Region } from 'react-native-maps';
 import Coordinates from 'locations/constants/cities';
+import { requestLocation } from 'common/permissions';
 
-class Map extends Component {
-    state = {
-        userLocation: {
-            ...Coordinates.London,
-        }
-    }
+function Map() {
+    const INITAL_REGION = {
+        ...Coordinates.London,
+        latitudeDelta: 0.1000,
+        longitudeDelta: 0.1000,
+    };
 
-    componentDidMount() {
-        this.requestLocationPermission();
-    }
+    const [ locationSet, setLocationSet ] = useState(false);
+    const mapRef: MutableRefObject<MapView | null> = useRef(null);
 
-    requestLocationPermission() {
-        Location.requestPermissionsAsync();
-    }
+    useEffect(requestLocation);
 
-    onUserLocationChange = (e: EventUserLocation) => {
-        const { coordinate } = e.nativeEvent;
-
-        this.setState({
-            userLocation: {
-                latitude: coordinate.latitude,
-                longitude: coordinate.longitude,
+    const onLocationChange = (e: EventUserLocation): void => {
+        if (locationSet === false) {
+            const { coordinate } = e.nativeEvent;
+            if (mapRef.current !== null) {
+                mapRef.current.animateToRegion({
+                    ...INITAL_REGION,
+                    latitude: coordinate.latitude,
+                    longitude: coordinate.longitude,
+                });
+                setLocationSet(true);
             }
-        })
-    }
+        }
+    };
 
-    render() {
-        const { userLocation } = this.state;
-
-        return (
-            <MapView style={styles.mapStyle}
-                initialRegion={{
-                    ...Coordinates.London,
-                    latitudeDelta: 0.1000,
-                    longitudeDelta: 0.1000,
-                }}
-                region={{
-                    ...userLocation,
-                    latitudeDelta: 0.1000,
-                    longitudeDelta: 0.1000,
-                }}
-                provider={PROVIDER_GOOGLE}
-                // TODO: Need to add config before deploying this see intructions in the repo:
-                // https://github.com/react-native-community/react-native-maps/blob/master/docs/mapview.md
-                showsUserLocation={true}
-                showsMyLocationButton={true}
-                onUserLocationChange={this.onUserLocationChange}
-                mapPadding={{
-                    top: 0,
-                    bottom: 80,
-                    left: 0,
-                    right: 0,
-                }}
-            />
-        );
-    }
+    return (
+        <MapView style={styles.mapStyle}
+            initialRegion={INITAL_REGION}
+            provider={PROVIDER_GOOGLE}
+            // TODO: Need to add config before deploying this see intructions in the repo:
+            // https://github.com/react-native-community/react-native-maps/blob/master/docs/mapview.md
+            showsUserLocation={true}
+            showsMyLocationButton={true}
+            onUserLocationChange={onLocationChange}
+            ref={mapRef}
+            mapPadding={{
+                top: 0,
+                bottom: 80,
+                left: 0,
+                right: 0,
+            }}
+        />
+    );
 }
 
 const styles = StyleSheet.create({
