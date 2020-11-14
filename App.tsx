71638dea6d60f5ea0b563780cbcb87dfcf1be3b8
John Judeh <johnjudeh@hotmail.com>
2020-11-14 22:52:02 +0000

    feat: Preload assets when the app is loading

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { AppLoading } from 'expo';
+import { useAssets } from 'expo-asset';
 import {
     useFonts,
     Roboto_400Regular,
@@ -13,7 +14,7 @@ import {
 import { AppContainer, store } from 'app';
 
 function App() {
-    const [fontsLoaded] = useFonts({
+    const [ fontsLoaded ] = useFonts({
         Roboto_400Regular,
         Roboto_400Regular_Italic,
         Roboto_500Medium,
@@ -21,8 +22,20 @@ function App() {
         Roboto_700Bold,
         Roboto_700Bold_Italic,
     });
+    const [ assets ] = useAssets([
+        require('categories/img/restaurant.jpg'),
+        require('categories/img/bar.jpg'),
+        require('categories/img/cafe.jpg'),
+        require('categories/img/museum.jpg'),
+        require('categories/img/park.jpg'),
+        require('categories/img/restaurant-large.jpg'),
+        require('categories/img/bar-large.jpg'),
+        require('categories/img/cafe-large.jpg'),
+        require('categories/img/museum-large.jpg'),
+        require('categories/img/park-large.jpg'),
+    ])
 
-    if (!fontsLoaded) {
+    if (!fontsLoaded || !assets) {
         return <AppLoading />;
     }
 
