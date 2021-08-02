2aab865f35238d1b82961a92cc3e97fcb968f1a8
John Judeh <johnjudeh@hotmail.com>
2021-08-02 20:19:21 +0100

    feat: Add shadown on Android using the LinearGradient component

--- src/common/components/Dock.tsx
+++ src/common/components/Dock.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Text, Platform } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import { ChildrenProps } from 'common/types';
 import Color from 'common/constants/colors';
 import FontFamily from 'common/constants/fonts';
@@ -10,39 +11,60 @@ interface DockProps extends ChildrenProps {
 }
 
 function Dock({ children, title, style }: DockProps) {
-    return (
-        <View style={[styles.dock, style]}>
-            {title
-                ? <View style={styles.titleContainer}>
-                    <Text style={styles.title}>{title}</Text>
-                </View>
-                : null
-            }
-            <View style={styles.childrenContainer}>
-                {children}
+    const dock = <View style={[styles.dock, style]}>
+        {title
+            ? <View style={styles.titleContainer}>
+                <Text style={styles.title}>{title}</Text>
             </View>
+            : null
+        }
+        <View style={styles.childrenContainer}>
+            {children}
         </View>
+    </View>;
+
+    return (
+        Platform.OS == 'ios'
+            ? dock
+            : <LinearGradient
+                // This component is used to create a shadow on
+                // Android as elevation is not featureful enough
+                colors={['transparent', Color.DARK_GREY]}
+                end={{ x: 0, y: 0.40 }}
+                style={styles.dockShadow}
+            >
+                {dock}
+            </LinearGradient>
     );
 }
 
 const styles = StyleSheet.create({
-    dock: {
+    dockShadow: {
         position: 'absolute',
         bottom: 0,
         width: '100%',
+        paddingTop: 7,
+        borderTopRightRadius: 5,
+        borderTopLeftRadius: 5,
+    },
+    dock: {
+        ...Platform.select({
+            ios: {
+                position: 'absolute',
+                bottom: 0,
+                width: '100%',
+                shadowOffset: {
+                    width: 0,
+                    height: -2,
+                },
+                shadowRadius: 5,
+                shadowOpacity: 0.20,
+            }
+        }),
         backgroundColor: Color.OFF_WHITE,
         borderTopRightRadius: 10,
         borderTopLeftRadius: 10,
         shadowColor: Color.DARK_GREY,
-        // Android only
-        elevation: 50,
-        // iOS only
-        shadowOffset: {
-            width: 0,
-            height: -2,
-        },
-        shadowRadius: 5,
-        shadowOpacity: 0.20,
     },
     titleContainer: {
         height: 56,
